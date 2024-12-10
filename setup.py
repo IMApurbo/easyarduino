94e6bfeca55014438431db20328a882617e73b62
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-12-10 22:38:39 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
+
 from setuptools import setup, find_packages
 
 setup(
-    name='easyarduino',  # Name of your package
-    version='0.1',  # Initial version of the package
-    description='A simple and easy-to-use Arduino control library using PyFirmata',
-    author='AKM Korishee Apurbo',  # Your name or the name of your organization
-    author_email='bandinvisible8@gmail.com',  # Your email
-    url='https://github.com/IMApurbo/easyarduino',  # Link to your repository or website
-    packages=find_packages(),  # Automatically finds all packages in the directory
-    install_requires=[  # List the dependencies required by your package
-        'pyfirmata',
-        'pyserial',  # pyserial is required for serial communication
+    name="easyarduino",
+    version="0.2",
+    description="A simple and easy-to-use Arduino control library using PyFirmata",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="AKM Korishee Apurbo",
+    author_email="bandinvisible8@gmail.com",
+    url="https://github.com/IMApurbo/easyarduino",
+    packages=find_packages(),
+    install_requires=[
+        "pyfirmata",
+        "pyserial",
     ],
-    classifiers=[  # Optional classifiers to help others find your package
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Minimum required Python version
+    python_requires=">=3.6",
 )
